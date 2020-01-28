387c5ce4172a3ec878f0ca9a174eedeb6739ad99
Platon Malyugin <malugin.p@gmail.com>
2020-01-28 21:38:59 +0700

    Add fetch transaction receipt to transaction service

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -3,10 +3,15 @@ import 'package:gogreen/models/transaction_model.dart';
 
 abstract class ITransactionService {
   Future<List<Transaction>> fetchTransactions(TransactionListFilter filter);
+  Future<String> fetchReceipt();
 }
 
 class TransactionService implements ITransactionService {
 
   @override
   Future<List<Transaction>> fetchTransactions(TransactionListFilter filter) => Future.value([]);
+
+  @override
+  Future<String> fetchReceipt() => Future.value("<h1>Hello World</h1>");
+
 }
